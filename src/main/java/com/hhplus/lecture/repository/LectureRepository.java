e9404cb5e0d0d76c4e96a37f60198beddae0d1e5
sunmon <blackmilk274@gmail.com>
2024-10-03 19:14:11 +0900

    feat: Lecture findById 예외처리 추가
    - 엔티티를 찾지 못하면 예외를 던지도록 변경

--- src/main/java/com/hhplus/lecture/repository/LectureRepository.java
+++ src/main/java/com/hhplus/lecture/repository/LectureRepository.java
@@ -1,10 +1,11 @@
 package com.hhplus.lecture.repository;
 
+import com.hhplus.lecture.common.LectureException;
 import com.hhplus.lecture.domain.Lecture;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureRepository {
-    Lecture findById(Long lectureId);
+    Lecture findById(Long lectureId) throws LectureException;
 
 }
--- src/main/java/com/hhplus/lecture/repository/LectureRepositoryImpl.java
+++ src/main/java/com/hhplus/lecture/repository/LectureRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.hhplus.lecture.repository;
 
+import com.hhplus.lecture.common.ErrorCode;
+import com.hhplus.lecture.common.LectureException;
 import com.hhplus.lecture.domain.Lecture;
 import com.hhplus.lecture.entity.LectureEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,10 @@ public class LectureRepositoryImpl implements LectureRepository {
     }
 
     @Override
-    public Lecture findById(Long lectureId) {
-        LectureEntity lectureEntity = lectureJpaRepository.findById(lectureId).get();
+    public Lecture findById(Long lectureId) throws LectureException {
+        LectureEntity lectureEntity = lectureJpaRepository.findById(lectureId)
+                .orElseThrow(() -> new LectureException(ErrorCode.LECTURE_NOT_FOUND, lectureId));
+
         return new Lecture(lectureEntity);
     }
 }
